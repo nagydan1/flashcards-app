73afa3b8c35f0b15a16664e6c6f5cce20e652102
nagydan1 <daniel.nagy.w@gmail.com>
2022-07-18 17:28:08 +0100

    user data validation and error handling

--- backend/src/api/users/user-model.js
+++ backend/src/api/users/user-model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { isEmail, isStrongPassword } from 'validator';
 
 const UserSchema = new mongoose.Schema({
   firstName: {
@@ -13,10 +14,18 @@ const UserSchema = new mongoose.Schema({
     type: String,
     unique: true,
     required: true,
+    validate: {
+      validator: isEmail,
+      message: 'Invalid e-mail address',
+    },
   },
   password: {
     type: String,
     required: true,
+    validate: {
+      validator: (password) => isStrongPassword(password, { minSymbols: 0 }),
+      message: 'Password must be at least 8 characters, incl. 1 number, 1 uppercase and 1 lowercase letter.',
+    },
   },
 });
 
