ed1f2b38e956f5284768f9191e6197801930edc2
nagydan1 <daniel.nagy.w@gmail.com>
2022-07-19 15:16:02 +0100

    auth register route documentation

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,21 @@
 import express from 'express';
 import morgan from 'morgan';
 
+import swaggerJsDoc from 'swagger-jsdoc'
+import swaggerUI from 'swagger-ui-express';
+import { swaggerSettings } from './config';
+
 import logger from './logger';
 import apiRoutes from './api/api-routes';
 import authRoutes from './auth/auth-routes';
 import errorHandler from './middlewares/error-handler';
 
 const app = express();
+const specs = swaggerJsDoc(swaggerSettings);
 
 app.use(morgan('combined', { stream: logger.stream }));
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use('/api', apiRoutes);
 app.use('/auth', authRoutes);
